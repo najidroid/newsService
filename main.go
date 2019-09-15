8a8d24ad4c43462e816ee4620d42b56fe0aac2df
vahid <naji.droid@gmail.com>
2019-09-15 11:52:34 +0430

    final

--- main.go
+++ main.go
@@ -44,10 +44,10 @@ func main() {
 	name := "default"
 
 	// Drop table and re-create.
-	force := false
+	force := true
 
 	// Print log.
-	verbose := false
+	verbose := true
 
 	// Error.
 	err := orm.RunSyncdb(name, force, verbose)
